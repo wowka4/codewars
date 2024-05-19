88a2007c9cb13d3f579bcee25c99794358f3338b
WoWka <vet-family@yandex.ru>
2024-05-19 13:07:34 +0300

    without-last.js

---
+++ 7kyu/without-last.js
@@ -0,0 +1,18 @@
+/* In this kata the function returns an array/list of numbers without its last element.
+The function is already written for you and the basic tests pass, but random tests fail.
+Your task is to figure out why and fix it.
+function withoutLast(arr) {
+  // Fix it
+  arr.pop(); // removes the last element
+  return arr;
+} */
+
+// Solution
+
+function solution(arr) {
+    // Fix it
+    // arr.pop(); // removes the last element
+
+}
+
+console.log('Solution: ', solution([1,2,3,6,77,90,4]));
