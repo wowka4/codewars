506b3b1b20e647a390cc3195252e200c85fc5b75
WoWka <vet-family@yandex.ru>
2024-05-20 16:34:21 +0300

    Chuck-norris-true-or-false.js

---
+++ 8kyu/Chuck-norris-true-or-false.js
@@ -0,0 +1,11 @@
+// Description:
+// Your task is to be more like Chuck (ha! good luck!).
+// You must return false without ever actually using the word false...
+
+//Solution:
+
+function solution(){
+    return '' === true;
+}
+
+console.log(solution()); // false
