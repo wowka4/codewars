1cec8b79bdafe2b9225a7a41dc827e4b0e7d15c0
WoWka <vet-family@yandex.ru>
2024-05-25 15:41:02 +0300

    Vowel-Count.js

---
+++ 7kyu/Vowel-Count.js
@@ -0,0 +1,19 @@
+/*Description:
+* Return the number (count) of vowels in the given string.
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+The input string will only consist of lower case letters and/or spaces.
+
+*/
+// Solution:
+
+function getCount(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    for (const char of str) {
+        if (vowels.includes(char)) count++;
+    }
+    return count;
+}
+
+console.log(getCount('abracadabra')); // 5
+
