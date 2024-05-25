4c08a11564b005204029b4612ef41699670ecf04
WoWka <vet-family@yandex.ru>
2024-05-25 16:12:29 +0300

    Highest-and-Lowest.js

---
+++ 7kyu/Highest-and-Lowest.js
@@ -0,0 +1,14 @@
+/*In this little assignment you are given a string of space separated numbers, and have to return the highest
+and lowest number.
+Examples:
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"*/
+// Solution:
+function highAndLow(numbers){
+    const min = Math.min(...numbers.toString().split(' ').map(Number));
+    const max = Math.max(...numbers.toString().split(' ').map(Number));
+    return `${max} ${min}`;
+}
+
+console.log(highAndLow('1 4 5 0 -1'));
