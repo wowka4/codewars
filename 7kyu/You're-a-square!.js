810380a7de48f734b4ee4514eb5c56455885b0d2
WoWka <vet-family@yandex.ru>
2024-05-26 15:50:21 +0300

    You're-a-square!.js

---
+++ 7kyu/You're-a-square!.js
@@ -0,0 +1,19 @@
+/*Task
+Given an integral number, determine if it's a square number:
+In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+Examples
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false*/
+// Solution:
+
+const isSquare = function (n) {
+    if (n === 0) return true;
+    return n % Math.sqrt(n) === 0; // fix me
+};
+
+console.log(isSquare(25));
