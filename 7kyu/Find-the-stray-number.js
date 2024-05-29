d045e1843b47b97f3d275b41d1e5d27287be0478
WoWka <vet-family@yandex.ru>
2024-05-29 20:04:13 +0300

    Find-the-stray-number.js

---
+++ 7kyu/Find-the-stray-number.js
@@ -0,0 +1,13 @@
+/*You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+Complete the method which accepts such an array, and returns that single different number.
+The input array will always be valid! (odd-length >= 3)
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3*/
+// Solution:
+
+function stray(numbers) {
+    return Number(numbers.filter(a => numbers.indexOf(a) === numbers.lastIndexOf(a)));
+}
+
+console.log(stray([17, 17, 3, 17, 17, 17, 17]));
