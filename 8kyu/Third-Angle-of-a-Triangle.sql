fe7d643576b9d071ef5bde0f7c61a2b0863cade7
Vladimir Chernenko <vet-family@yandex.ru>
2024-08-15 19:27:52 +0300

    Third-Angle-of-a-Triangle.sql

---
+++ 8kyu/Third-Angle-of-a-Triangle.sql
@@ -0,0 +1,8 @@
+-- You are given two interior angles (in degrees) of a triangle.
+-- Write a function to return the 3rd.
+-- Note: only positive integers will be tested.
+
+--# write your SQL statement here: 
+-- you are given a table 'otherangle' with columns 'a' and 'b'.
+-- return a table with these columns and your result in a column named 'res'.
+select a, b, 180 - (a + b) as res from otherangle
