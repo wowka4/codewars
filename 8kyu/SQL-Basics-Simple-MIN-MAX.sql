48d87f7f936275a79d51b54263ae85b521e7f172
Vladimir Chernenko <vet-family@yandex.ru>
2024-08-15 19:38:34 +0300

    SQL-Basics-Simple-MIN-MAX.sql

---
+++ 8kyu/SQL-Basics-Simple-MIN-MAX.sql
@@ -0,0 +1,7 @@
+-- For this challenge you need to create a simple MIN / MAX statement that will 
+-- return the Minimum and Maximum ages out of all the people.
+
+-- Create your SELECT statement here
+SELECT MIN(age) AS age_min,
+       MAX(age) AS age_max
+FROM people
