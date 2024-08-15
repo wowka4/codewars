f32f8359d4ad825d88956fb2f75e24f549d95b18
Vladimir Chernenko <vet-family@yandex.ru>
2024-08-15 19:33:24 +0300

    SQL-Basics-Simple-SUM.sql

---
+++ 8kyu/SQL-Basics-Simple-SUM.sql
@@ -0,0 +1,10 @@
+--For this challenge you need to create a simple SUM statement that will sum all the ages.
+--people table schema
+--id
+--name
+--age
+select table schema
+--age_sum (sum of ages)
+
+-- Create your SELECT statement here
+select SUM(age) as age_sum from people
