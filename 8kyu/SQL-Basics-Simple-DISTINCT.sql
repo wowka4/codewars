30f750cd21caebc82ee419a228dee82fb78073d1
Vladimir Chernenko <vet-family@yandex.ru>
2024-08-15 19:42:39 +0300

    SQL-Basics-Simple-DISTINCT.sql

---
+++ 8kyu/SQL-Basics-Simple-DISTINCT.sql
@@ -0,0 +1,7 @@
+-- For this challenge you need to create a simple DISTINCT statement,
+-- you want to find all the unique ages.
+
+-- Create your SELECT statement here
+SELECT
+    DISTINCT age
+FROM people
