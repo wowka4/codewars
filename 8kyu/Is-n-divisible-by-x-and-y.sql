f094de1007a72c41d87c7b04a9e5558d1b8e6b11
Vladimir Chernenko <vet-family@yandex.ru>
2024-08-17 15:34:33 +0300

    Is-n-divisible-by-x-and-y.sql

---
+++ 8kyu/Is-n-divisible-by-x-and-y.sql
@@ -0,0 +1,12 @@
+/*Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+
+Examples:
+1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+4) n =  12, x = 7, y = 5 => false because  12 is neither divisib*/
+
+-- You will be given a table 'kata' with columns 'id', 'n', 'x', and 'y'. 
+-- Return the 'id' and your result in a column named 'res' using a SELECT statement.
+-- You DON'T need to create a function, that's for other languages.
+select id, n % x = 0 AND n % y = 0 AS res from kata
